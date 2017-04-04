a3cfd5c4dadca2cc19bb57069ffe6f0ad6866cbe
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2017-04-04 16:38:10 +0000

    add day 3 components, rudimentary styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,15 @@ import { ContactMeComponent } from './contact-me/contact-me.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { MyContactsComponent } from './my-contacts/my-contacts.component';
 import { MyCalendarComponent } from './my-calendar/my-calendar.component';
+import { ProjectComponent } from './project/project.component';
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
+import { TaskComponent } from './task/task.component';
+import { ContactComponent } from './contact/contact.component';
+import { ContactDetailComponent } from './contact-detail/contact-detail.component';
+import { MonthComponent } from './month/month.component';
+import { WeekComponent } from './week/week.component';
+import { DayComponent } from './day/day.component';
+import { DayDetailComponent } from './day-detail/day-detail.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +29,16 @@ import { MyCalendarComponent } from './my-calendar/my-calendar.component';
     ContactMeComponent,
     TodoListComponent,
     MyContactsComponent,
-    MyCalendarComponent
+    MyCalendarComponent,
+    ProjectComponent,
+    ProjectDetailComponent,
+    TaskComponent,
+    ContactComponent,
+    ContactDetailComponent,
+    MonthComponent,
+    WeekComponent,
+    DayComponent,
+    DayDetailComponent
   ],
   imports: [
     BrowserModule,
